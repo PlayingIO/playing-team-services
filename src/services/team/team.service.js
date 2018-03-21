b3743f3fda1f5486ea43143f102571ca74991725
forafish <fora.luo@gmail.com>
2018-03-21 16:18:22 +0800

    force space-before-function-paren

--- src/services/team/team.service.js
+++ src/services/team/team.service.js
@@ -12,18 +12,18 @@ const defaultOptions = {
 };
 
 class TeamService extends Service {
-  constructor(options) {
+  constructor (options) {
     options = Object.assign({}, defaultOptions, options);
     super(options);
   }
 
-  setup(app) {
+  setup (app) {
     super.setup(app);
     this.hooks(defaultHooks(this.options));
   }
 }
 
-export default function init(app, options, hooks) {
+export default function init (app, options, hooks) {
   options = Object.assign({ ModelName: 'team' }, options);
   return createService(app, TeamService, TeamModel, options);
 }
