868baf87edbe73207e4757fd626d3a1d608d9dff
forafish <fora.luo@gmail.com>
2018-03-23 18:18:21 +0800

    export default hooks

--- src/services/team/team.hooks.js
+++ src/services/team/team.hooks.js
@@ -3,7 +3,7 @@ import { cache } from 'mostly-feathers-cache';
 
 import TeamEntity from '~/entities/team.entity';
 
-module.exports = function (options = {}) {
+export default function (options = {}) {
   return {
     before: {
       all: [
