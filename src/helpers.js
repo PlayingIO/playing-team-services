a9ad7aa690927efdcb12d9ba8c84c18cf1dabc8a
forafish <fora.luo@gmail.com>
2018-05-21 14:47:59 +0800

    add helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,25 @@
 import fp from 'mostly-func';
+import { helpers } from 'mostly-feathers-mongoose';
+import { helpers as rules } from 'playing-rule-services';
 
-export default {};
+export const fulfillTeamRequires = (team, user) => {
+  return rules.fulfillRequires(user, [], team.settings.requires);
+};
+
+// create a group activity
+export const createTeamActivity = (context, team, custom) => {
+  const actor = helpers.getId(team.owner);
+  return {
+    actor: `user:${actor}`,
+    object: `team-design:${team.definition}`,
+    foreignId: `${team.type}:${team.id}`,
+    time: new Date().toISOString(),
+    ...custom
+  };
+};
+
+// notification feeds of all team members
+export const membersNotifications = function (members, excepts = []) {
+  const users = fp.without(excepts, fp.map(fp.prop('user'), members || []));
+  return fp.map(fp.concat('notification:'), users);
+};
