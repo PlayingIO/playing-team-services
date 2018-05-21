cd4a5745b2c4175ae2d0a5503114341b88b2848a
forafish <fora.luo@gmail.com>
2018-05-21 14:47:29 +0800

    add team model

---
+++ src/models/team.model.js
@@ -0,0 +1,16 @@
+/**
+ * Group created by team definition
+ */
+const fields = {
+  definition: { type: 'ObjectId' },        // team definition
+  access: { type: String, enum: [          // visibility setting
+    'PUBLIC', 'PROTECTED', 'PRIVATE'
+  ]}
+};
+
+export default function model (app, name) {
+  const mongoose = app.get('mongoose');
+  const GroupModel = mongoose.model('group');
+  const schema = new mongoose.Schema(fields);
+  return GroupModel.discriminator(name, schema);
+}
