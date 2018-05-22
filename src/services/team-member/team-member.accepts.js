8b19a1702d4e5f8cfc5f4371e42ca665d2c67377
forafish <fora.luo@gmail.com>
2018-05-23 02:52:21 +0800

    Fix rolesExists validation for team roles

--- src/services/team-member/team-member.accepts.js
+++ src/services/team-member/team-member.accepts.js
@@ -1,12 +1,14 @@
 import fp from 'mostly-func';
 import { helpers } from 'mostly-feathers-validate';
+import { rolesExists } from '../../helpers';
 
 export default function accepts (context) {
+  const svcTeams = context.app.service('teams');
   const svcUsers = context.app.service('users');
 
   // validation rules
   const roles = { arg: 'roles', type: 'object',
-    validates: { exists: helpers.isInParams('team.roles', 'Roles is invalid') },
+    validates: { exists: rolesExists(svcTeams, 'primary', 'Roles is invalid') },
     required: true, description: 'Roles ' };
 
   return {
