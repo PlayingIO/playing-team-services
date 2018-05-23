7ba517c5715ea156bcc6cc3793614ff48fd3679d
forafish <fora.luo@gmail.com>
2018-05-23 16:50:43 +0800

    notify mission.join activity

---
+++ src/services/team-member/team-member.notifiers.js
@@ -0,0 +1,43 @@
+import fp from 'mostly-func';
+import { helpers } from 'mostly-feathers-mongoose';
+
+import { createTeamActivity, membersNotifications } from '../../helpers';
+
+// join team activity
+const joinTeam = (context) => {
+  const { team } = context.params.locals;
+  const actor = context.params.user.id;
+  if (team.access === 'PUBLIC') {
+    const notifications = membersNotifications(team.members);
+    const custom = {
+      actor: `user:${actor}`,
+      verb: 'team.join',
+      message: 'Join the team',
+      roles: context.data.roles
+    };
+    return [
+      createTeamActivity(context, team, custom),
+      `user:${actor}`,               // add to player's activity log
+      `user:${team.owner}`,          // add to owner's activity log
+      `team:${team.id}`,             // add to team's activity log
+      notifications                  // add to all members' notification stream
+    ];
+  } else {
+    const custom = {
+      actor: `user:${actor}`,
+      verb: 'team.join.request',
+      message: 'Request join the team',
+      roles: context.data.roles,
+      state: 'PENDING'
+    };
+    return [
+      createTeamActivity(context, team, custom),
+      `user:${actor}`,               // add to player's activity log
+      `notification:${team.owner}`   // notify owner of the team to approve requests
+    ];
+  }
+};
+
+export default {
+  'team.join': joinTeam,
+};
