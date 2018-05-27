d8d8683201b9002b27ed77e514b9851dac7ad5b8
forafish <fora.luo@gmail.com>
2018-05-27 18:42:46 +0800

    notify group.transfer activity

--- src/services/team/team.notifiers.js
+++ src/services/team/team.notifiers.js
@@ -39,7 +39,31 @@ const deleteTeam = (context) => {
   ];
 };
 
+// transfer ownership of mission activity
+const transferTeam = (context) => {
+  const team = helpers.getHookData(context);
+  if (!team) return;
+  const actor = context.params.user.id;
+  const newOwner = context.data.player;
+  const notifications = membersNotifications(team.members);
+  const custom = {
+    actor: `user:${actor}`,
+    verb: 'group.transfer',
+    message: 'Transfer the ownership of the team',
+    roles: context.data.roles,
+    newOwner: `user:${newOwner}`
+  };
+  return [
+    createTeamActivity(context, team, custom),
+    `user:${actor}`,               // add to old owner's activity log
+    `user:${newOwner}`,            // add to new owner's activity log
+    `team:${team.id}`,             // add to mission's activity log
+    notifications                  // add to all members' notification stream
+  ];
+};
+
 export default {
   'group.create': createTeam,
-  'group.delete': deleteTeam
+  'group.delete': deleteTeam,
+  'group.transfer': transferTeam
 };
