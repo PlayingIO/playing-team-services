110de8bd3dbfcea3a436a0a7af195fdb72ac88aa
forafish <fora.luo@gmail.com>
2018-05-30 17:03:52 +0800

    use playing-content-common

--- src/entities/team-design.entity.js
+++ src/entities/team-design.entity.js
@@ -1,8 +1,8 @@
 import Entity from 'mostly-entity';
-import { entities as contents } from 'playing-content-services';
+import { BlobEntity } from 'playing-content-common';
 
 const TeamDesignEntity = new Entity('TeamDesign', {
-  image: { using: contents.BlobEntity }
+  image: { using: BlobEntity }
 });
 
 TeamDesignEntity.excepts('updatedAt', 'destroyedAt');
