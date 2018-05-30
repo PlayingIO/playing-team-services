cbd3d566b62bceb1412eb38a357210d24bd370ab
forafish <fora.luo@gmail.com>
2018-05-30 18:53:41 +0800

    use blob schema from playing-content-common

--- src/models/team-design.model.js
+++ src/models/team-design.model.js
@@ -1,5 +1,5 @@
 import { plugins } from 'mostly-feathers-mongoose';
-import { models as contents } from 'playing-content-services';
+import { schemas as contents } from 'playing-content-common';
 import { schemas as rules } from 'playing-rule-common';
 
 const options = {
